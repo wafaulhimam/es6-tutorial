a23720eda05f6cca7ab0b120998d4ea3301d0e45
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2018-11-07 21:09:53 +0700

    Commit pertama

---
+++ es6-tutorial/lib.js
@@ -0,0 +1,7 @@
+export const x = 2;
+export function multiply(a,b){
+  return a * b;
+}
+export function multiplyWithx(y){
+  return x * y;
+}
---
+++ es6-tutorial/main.js
@@ -0,0 +1,3 @@
+import { multiply, multiplyWithx } from './lib';
+console.log(multiply(5,5)); // 25
+console.log(multiplyWithx(6)); // 12
