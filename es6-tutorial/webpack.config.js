71a354efdbba9bc260e1c41a15302235c2213cb9
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2018-11-08 00:48:33 +0700

    edit index biar bagus

--- es6-tutorial/webpack.config.js
+++ es6-tutorial/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'main.bundle.js'
   }
 };
